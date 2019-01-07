658ad621af6e40968ec0094bb595fd8cb0993fec
gengkang <gengkang@live.com>
2019-01-07 11:52:58 +0800

    golang xml to map

---
+++ README.md
@@ -0,0 +1,28 @@
+# xml unmarsahl to map for golang
+
+>It's a Demo project, edit your __UnmarshalXML__ method to change behaviour
+
+## usage
+
+```golang
+package main
+
+import (
+    xmltomap "github.com/nelsonken/xmltomap-go"
+)
+
+func main() {
+    data := []byte(`<xml>
+<a>1</a>
+<b>hi hello world</b>
+<c>xxxx</c>
+<c>rrr</c>
+<xml>`)
+    strmap, err := xmltomap.Unmarshal(data)
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Println(strmap)
+}
+```
---
+++ xml_to_map.go
@@ -0,0 +1,61 @@
+package xmltomap
+
+import "encoding/xml"
+
+func UnmarshalXMLToMap(xmlData []byte) (map[string]string, error) {
+	u := AccessesMap{}
+	err := xml.Unmarshal(xmlData, &u)
+	return u.M, err
+}
+
+type AccessesMap struct {
+	M map[string]string
+}
+
+func (am *AccessesMap) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	am.M = map[string]string{}
+
+	for {
+		t, err := d.Token()
+		if err != nil {
+			return err
+		}
+		switch tt := t.(type) {
+		case xml.StartElement:
+			if tt.Name.Local == "" {
+				continue
+			}
+
+			v, err := nextTokenValue(d)
+			if err != nil {
+				return err
+			}
+
+			if mv, ok := am.M[tt.Name.Local]; ok {
+				v = mv + "," + v
+			}
+
+			am.M[tt.Name.Local] = v
+		}
+	}
+
+	return nil
+}
+
+func nextTokenValue(d *xml.Decoder) (string, error) {
+	val := ""
+	b := false
+	for !b {
+		token, err := d.Token()
+		if err != nil {
+			break
+		}
+		switch x := token.(type) {
+		case xml.EndElement:
+			b = true
+		case xml.CharData:
+			val += string(x)
+		}
+	}
+	return val, nil
+}
